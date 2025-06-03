d60639a997244e96f0738dcc47c9f18d73f033af
rober0xf <robertinopili13@gmail.com>
2025-06-03 14:09:49 -0300

    app folder

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
-from django.conf.global_settings import MEDIA_ROOT
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
