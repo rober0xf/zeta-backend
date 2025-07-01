29ab4a31db614d0f1953defe3ed57b9879f8ce19
rober0xf <robertinopili13@gmail.com>
2025-07-01 20:03:10 -0300

    post urls: search by filter

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
-from .views import PostsListView
 
-urlpatterns = [path("posts", PostsListView.as_view())]
+from .views import ListPostByCategory, PostDetailView, PostsListView, SearchBlogView
+
+urlpatterns = [
+    path("list/", PostsListView.as_view()),
+    path("category/", ListPostByCategory.as_view()),
+    path("detail/<slug>", PostDetailView.as_view()),
+    path("search", SearchBlogView.as_view()),
+]
